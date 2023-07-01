64e558505d11b3fcc31e23b901439afe570999b3
gascione <gaglausiuss@gmail.com>
2023-07-01 16:47:49 -0300

    Building game cards

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -3,9 +3,10 @@ import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
 
 //definimos la interface de c/Game
-interface Game {
+export interface Game {
   id: number;
   name: string;
+  background_image: string;
 }
 
 //definimos el interface de cmo vamos a recibir la response
