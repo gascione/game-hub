3cab53ed29fd828db76e406a2e62b7c20ebe6293
gascione <gaglausiuss@gmail.com>
2023-07-01 19:33:53 -0300

    Displaying platform icons

---
+++ src/components/PlatformIconList.tsx
@@ -0,0 +1,44 @@
+import {
+  FaWindows,
+  FaPlaystation,
+  FaXbox,
+  FaApple,
+  FaLinux,
+  FaAndroid,
+} from "react-icons/fa";
+import { MdPhoneIphone } from "react-icons/md";
+import { SiNintendo } from "react-icons/si";
+import { BsGlobe } from "react-icons/bs";
+import { HStack, Icon, Text } from "@chakra-ui/react";
+import { Platform } from "../hooks/useGames";
+import { IconType } from "react-icons";
+
+interface Props {
+  platforms: Platform[];
+}
+const PlatformIconList = ({ platforms }: Props) => {
+  //mapeamos cada icono con el nombre, xa que no nos de error tenemos que poner que es un objeto que va a tener any number of string keys (string signature)
+  const iconMap: { [key: string]: IconType } = {
+    pc: FaWindows,
+    playstation: FaPlaystation,
+    xbox: FaXbox,
+    nintendo: SiNintendo,
+    mac: FaApple,
+    android: FaAndroid,
+    linux: FaLinux,
+    ios: MdPhoneIphone,
+    web: BsGlobe,
+  };
+  return (
+    // ahi escribimos {1} que corresponde a 1 medida base definida por chakra, si queremos un valor preciso ponemos {'10px'}
+    <HStack marginY={1}>
+      {/* destructuramos platform asi no tenemos que escribir platform.platform */}
+      {platforms.map((platform) => (
+        // xa renderizar los iconos de manera dinamica, usamos el slug porque siempre es igual y esta en minuscula
+        <Icon as={iconMap[platform.slug]} color="gray.500"></Icon>
+      ))}
+    </HStack>
+  );
+};
+
+export default PlatformIconList;
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,11 +2,20 @@ import { CanceledError } from "axios";
 import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
 
+//platform object
+
+export interface Platform {
+  id: number;
+  name: string;
+  slug: string;
+}
 //definimos la interface de c/Game
 export interface Game {
   id: number;
   name: string;
   background_image: string;
+  //array of objects where each object has a platform property of type Platform
+  parent_platforms: { platform: Platform }[];
 }
 
 //definimos el interface de cmo vamos a recibir la response
