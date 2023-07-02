fd719c7fbb38205da24736065f0d380fb75c5276
gascione <gaglausiuss@gmail.com>
2023-07-02 20:32:58 -0300

    Handle games with no image

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,6 +1,8 @@
+import noImage from "../assets/no-image-placeholder.jpeg";
+
 //esta funcion agarra la url original y devuelve otra pero modificada, con la opcion crop xa que sea mas pequeña la imagen
 const getCroppedImageUrl = (url: string) => {
-  if (!url) return "";
+  if (!url) return noImage;
   //buscamos la parte que dice media
   //indexOf nos devuelve el index donde empieza lo que buscamos entonces agregamos el valor del lenght de media/, asi podemos agregar en el numero correcto crop
   //como repetimos muchas veces media/, hacemos una variable a parte xa q sea + prolijo
