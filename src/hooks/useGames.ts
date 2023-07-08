8ae5bf0795a599d71f6f91176bcf0313b251a86e
gascione <gaglausiuss@gmail.com>
2023-07-07 23:15:15 -0300

    Refactoring- fetch games with React Query

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,6 +1,7 @@
+import { useQuery } from "@tanstack/react-query";
 import { GameQuery } from "../App";
-import useData from "./useData";
-import { Genre } from "./useGenres";
+import apiClient from "../services/api-client";
+import { FetchResponse } from "../services/api-client";
 
 //platform object
 
@@ -9,6 +10,7 @@ export interface Platform {
   name: string;
   slug: string;
 }
+
 //definimos la interface de c/Game
 export interface Game {
   id: number;
@@ -21,17 +23,19 @@ export interface Game {
 }
 
 const useGames = (gameQuery: GameQuery) =>
-  // como selectedGenre puede ser null entonces ponemos selectedGenre?.id
-  useData<Game>(
-    "/games",
-    {
-      params: {
-        genres: gameQuery.genre?.id,
-        platforms: gameQuery.platform?.id,
-        ordering: gameQuery.sortOrder,
-        search: gameQuery.searchText,
-      },
-    },
-    [gameQuery]
-  );
+  useQuery<FetchResponse<Game>, Error>({
+    queryKey: ["games", gameQuery],
+    queryFn: () =>
+      apiClient
+        .get<FetchResponse<Game>>("/games", {
+          params: {
+            genres: gameQuery.genre?.id,
+            parent_platforms: gameQuery.platform?.id,
+            ordering: gameQuery.sortOrder,
+            search: gameQuery.searchText,
+          },
+        })
+        .then((res) => res.data),
+  });
+
 export default useGames;
